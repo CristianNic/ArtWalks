e227079ab25904a02c4993ae486e7012aafc126b
Cristian <cristian.niculescu@gmail.com>
2021-03-09 00:33:42 -0800

    first chart

---
+++ app/src/draft.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Plot from 'react-plotly.js';
+
+class Graphs extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          
+        </header>
+      </div>
+    )
+  }
+}
+
+export default Graphs;
