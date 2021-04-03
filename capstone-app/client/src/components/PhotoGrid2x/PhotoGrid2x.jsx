6cf3293d8305d12e7c0204e3a06aff9cc8c7c34c
Cristian <cristian.niculescu@gmail.com>
2021-04-03 16:14:28 -0700

    extracted data from pairs

--- capstone-app/client/src/components/PhotoGrid2x/PhotoGrid2x.jsx
+++ capstone-app/client/src/components/PhotoGrid2x/PhotoGrid2x.jsx
@@ -4,6 +4,7 @@
 import { Component } from "react";
 import './PhotoGrid2x.scss'
 import BottomNav from '../BottomNav/BottomNav';
+import LazyLoad from 'react-lazy-load';
 // import PhotoGridModal from '../PhotoGridModal/PhotoGridModal';
 // import ArtWorks from '../ArtWorks/ArtWorks';     /// was version 2 
 import Search from '../Search/Search';
@@ -32,6 +33,7 @@ import artWorksData from '../../data_temp/art_work'
 
 import heartRed from '../../assets/icons/heart_red.svg';
 import heartWhite from '../../assets/icons/heart_white.svg';
+import iconMap from '../../assets/icons-feather-1.5px/map.svg';
 
 
 // const img1 = "https://picsum.photos/id/237/650/400";
@@ -83,6 +85,9 @@ class PhotoGrid2x extends Component {
   //   });
   //   console.log('can I add more features?', this.state.display)
   // };
+  componentDidMount() {
+    this.reduceArrayIntoPairs(artWorksData)
+  }
 
   handleButtonClick = (e, data) => {
     e.currentTarget.style.boxshadow = "none";
@@ -99,21 +104,23 @@ class PhotoGrid2x extends Component {
   };
   
   reduceArrayIntoPairs = (artWorksData) => {
-    console.log(artWorksData);
     const rows = artWorksData.reduce(function (rows, key, index) {
 	    return (
 		    (index % 2 == 0 ? rows.push([key]) : rows[rows.length - 1].push(key)) &&
 		    rows
     	);
     }, []);
-    console.log(rows);
+    //console.log(rows);
+    return rows; // <--- map rows
   } 
 
 
   render() {
 
-    console.log('ArtWorks Imported Obj --> ', artWorksData);
-
+    console.log('ArtWorks Imported Obj -->', artWorksData);
+    console.log('ArtWorks Imported Obj -- grouped -->', this.reduceArrayIntoPairs(artWorksData));
+    const rows = this.reduceArrayIntoPairs(artWorksData)
+    console.log('Access Rows -->', rows[0][0])
     // reduceArrayIntoPairs(artWorksData);
 
     return (
@@ -122,39 +129,39 @@ class PhotoGrid2x extends Component {
         <div className="myfirst">
         {/* ------------ Click photo to expand display ------------*/}
         {/* --------------- Doubled Up ---------------*/}
-        
-        <div>
-        {/* <button className="btn" onClick={this.handleButtonClick2}> */}
-        {/* <button className="btn" onClick={(e) => { this.handleButtonClick2(e, this.props.id) }}> */}
-        <button className="btn" onClick={(e) => { this.handleButtonClick(e, 1) }}>
-          <div className="container">
-            <img className="img" src={img6}></img>
-            <img className="like filter-white" src={heartWhite}></img>
-          </div>
-        </button>
-        {/* <button className="btn" onClick={this.handleButtonClick2}> */}
-        {/* <button className="btn" onClick={(e) => { this.handleButtonClick2(e, this.state.item2) }}> */}
-        <button className="btn" onClick={(e) => { this.handleButtonClick(e, 2) }} >
-          <div className="container">
-            <img className="img" src={img7}></img>
-            <img className="like filter-white" src={heartWhite}></img>
-          </div>
-        </button>
+        <div className="btn-wrapper">
+          <button className="btn" onClick={(e) => { this.handleButtonClick(e, 1) }}>      
+            <div className="container">
+              <LazyLoad className="lazyload" offsetVertical={700} overflow={true} >
+                <img className="img" src={img6}></img>
+              </LazyLoad>
+              <img className="like filter-white" src={heartWhite}></img>
+            </div>
+          </button>
+          <button className="btn" onClick={(e) => { this.handleButtonClick(e, 2) }} >
+            <div className="container">
+              <LazyLoad className="lazyload" offsetVertical={700} overflow={true} >
+                <img className="img" src={img7}></img>
+              </LazyLoad>    
+              <img className="like filter-white" src={heartWhite}></img>
+            </div>
+          </button>
         </div>
-        <div>
-        {/* {this.state.display && this.state.expand === 7 && ( */}
         {this.state.display && this.state.expand === 1 && (
-          <div class="expander">
-            <ul>
+          <div className="expander">
+            <ul className="ul">
               {/* <h1>{this.prop.title0}Title img6</h1> */}
-              <h2>Artist 1</h2>
-              <h2>Neighbourhood</h2>
-              <h2>Medium</h2>
-              <h2>Statement</h2>
+              <h1>Artist 1</h1>
+              <h1>{this.artWorksData[0].title}</h1>
+              <img className="filter-white" src={iconMap}></img>
+              <h2>Neighbourhood Neighbourhood Neighbourhood</h2>
+              <h3>Medium Medium Medium Medium Medium Medium Medium Medium</h3>
+              <h4>Statement Statement Statement Statement Statement Statement</h4>
+              <p>Statement Statement Statement Statement Statement Statement</p>
             </ul>
           </div>)}
         {this.state.display && this.state.expand === 2 && (
-          <div class="expander">
+          <div className="expander">
             <ul>
               {/* <h1>{this.prop.title1}Title img7</h1> */}
               <h2>Artist 2</h2>
@@ -163,18 +170,15 @@ class PhotoGrid2x extends Component {
               <h2>Statement</h2>
             </ul>
           </div>)}
-        </div>
 
           {/* {(couple) => (itme2, item2)} */}
           
         {/* --------------- Grid ---------------*/}
 
-        <button className="btn" onClick={this.reduceArrayIntoPairs(artWorksData)} >
           <div className="container">
             <img className="img" src={img8}></img>
             <img className="like" src={heartRed}></img>
           </div>
-        </button>
           
         <div className="container">
           <img className="img" src={img9}></img>
