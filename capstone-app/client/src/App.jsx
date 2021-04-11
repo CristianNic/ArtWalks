324da3bf2de964d886984ae03b5cecc2062895bb
Cristian <cristian.niculescu@gmail.com>
2021-04-11 12:32:18 -0700

    more cleaning up

--- capstone-app/client/src/App.jsx
+++ capstone-app/client/src/App.jsx
@@ -6,12 +6,11 @@ import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import MapArt from './pages/MapArt';
 import Explore from './pages/Explore';
-import Saved from './pages/Saved';
+// import Saved from './pages/Saved';
 import Details from './pages/Details';
 import Profile from './pages/Profile';
 import LayeredMap from './pages/LayeredMap/LayeredMap';
 import LayeredMapChloro from './pages/LayeredMapChloro/LayeredMapChloro';
-import PhotoGrid from './components/PhotoGrid/PhotoGrid'
 import PhotoGrid2x from './components/PhotoGrid2x/PhotoGrid2x'
 import DropDown from './components/DropDown/DropDown'
 import PageNotFound from './components/PageNotFound/PageNotFound';
@@ -48,7 +47,6 @@ class App extends Component {
           <Route exact path='/profile/:id' component={Profile} />
           <Route exact path='/layered_map' component={LayeredMap} />
           <Route exact path='/layered_map_chloro' component={LayeredMapChloro} />
-          <Route exact path='/photo-grid' component={PhotoGrid} />
           <Route exact path='/photo-grid2x' component={PhotoGrid2x} />
           <Route exact path='/dropdown' component={DropDown} />
           <Route exact path='/*' component={PageNotFound} />
--- capstone-app/client/src/pages/Explore.jsx
+++ capstone-app/client/src/pages/Explore.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { Component } from 'react';
 // import { BrowserRouter as Link } from 'react-router-dom';
-import * as utils from '../components/utils';
+// import * as utils from '../components/utils';
 import BottomNav from '../components/BottomNav/BottomNav';
 import ArtWorks from '../components/ArtWorks/ArtWorks';     /// was version 2 
 import Search from '../components/Search/Search';
 
-const url = utils.API_URL;  //"http://localhost:8090/"
+// const url = utils.API_URL;  //"http://localhost:8090/"
 const apiUrlUser = 'http://localhost:8090/favourites/1'
 
 class Browse extends Component {
