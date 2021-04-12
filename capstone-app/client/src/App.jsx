1401adc4bae2ab34c97027bbff7e2acc26f6e7f0
Cristian <cristian.niculescu@gmail.com>
2021-04-12 16:40:21 -0700

    updated paths, exact /map

--- capstone-app/client/src/App.jsx
+++ capstone-app/client/src/App.jsx
@@ -1,11 +1,5 @@
 import { Component } from 'react';
-// import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-import { BrowserRouter as Router, Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import PropTypes from "prop-types";
-// import { withRouter } from 'react-router';
-import { API_URL } from './components/utils';
-import axios from 'axios';
-
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import MapArt from './pages/MapArt';
@@ -15,7 +9,6 @@ import Details from './pages/Details';
 import Profile from './pages/Profile';
 import Gallery from './components/Gallery/Gallery'
 import PageNotFound from './components/PageNotFound/PageNotFound';
-
 
 // Test pages: 
 // import LayeredMap from './pages/LayeredMap/LayeredMap';
@@ -32,24 +25,9 @@ class App extends Component {
           {/* <Route exact path='/login' component={Login} handleLogin={this.handleLogin()}/>   */}
           {/* <Route exact path='/login' render={props => <Login {...props} />} /> */}
           {/* <Route exact path='/login' render={props => <Login handleLogin = {this.handleLogin} />} /> */}
-          <Route exact path='/login'
-            render={props =>
-              <Login
-                handleLogin={this.handleLogin}
-                function={this.handleSaveUserOverHere}
-                // sendData={this.getData}
-              />
-            }
-          />
+          <Route exact path='/login' component={Login}/>
           <Route exact path='/signup' component={SignUp} />  
-          <Route path='/map' component={MapArt} />       
-          <Route path='/map'
-            render={props =>
-              <MapArt
-                user_id={this.state.user_id}
-              />
-            }
-          />
+          <Route exact path='/map' component={MapArt} />       
           <Route exact path='/art_works' component={Explore} />
           <Route exact path='/art_works/:id' component={Details} />
           {/* <Route exact path='/favourites' component={Saved} /> */}
@@ -65,15 +43,12 @@ class App extends Component {
 }
 export default App;
 
-// Login calls - gets data - then passes it to map and explore
-// how to set data on the parent component  
 
 // Auth
 // https://reactrouter.com/native/example/auth-workflow
 // https://auth0.com/blog/react-router-4-practical-tutorial/
 // https://auth0.com/blog/complete-guide-to-react-user-authentication/
 // http://www.passportjs.org/docs/configure/
-
 
 
 
