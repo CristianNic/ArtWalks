212f530f7974c66ae9cc546721750e342126d304
Cristian <cristian.niculescu@gmail.com>
2021-04-12 01:31:08 -0700

    redirects to map with user id

--- capstone-app/client/src/pages/Login.jsx
+++ capstone-app/client/src/pages/Login.jsx
@@ -1,29 +1,31 @@
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { API_URL } from '../components/utils';
 import BottomNav from '../components/BottomNav/BottomNav';
 
 class Login extends Component {
-
+  
   state = {
-    // email: 'Cristian.Niculescu@gmail.com',
-    // password: '123456789',
     email: '', // email: localStorage.getItem('email') || '',
-    password: '' // password: localStorage.getItem('password') || '',
-    // user_id: '',  // temp could have it here to pass to the next component
+    password: '', // password: localStorage.getItem('password') || '',
+    user_id: '',
   }
 
-  // Send login information to the API
   handleLogin = (event) => {
-    axios.post('http://localhost:8090/login', this.state)
+    event.preventDefault();
+    axios.post(`${API_URL}/login`, this.state)
       .then(response => {
-        console.log(response.data);
+        console.log('user_id', response.data);
         // Store the auth token by the server in your session storage
         // sessionStorage.authToken = response.data.token;
+        this.setState({
+          user_id: response.data.user_id,
+        })
         
-        // debugger
-        this.props.history.push('/map');
-        // Get current location here or on map? 
+        window.location = `http://localhost:3000/map/${this.state.user_id}`;
+
+        // Get current url location
         // https://surajsharma.net/blog/current-url-in-react
         // https://reactrouter.com/web/api/NavLink
         
@@ -47,13 +49,10 @@ class Login extends Component {
     // localStorage.setItem(event.target.name, event.target.value);  // place them here - and then they become state to be passed along ... 
   }
 
-  componentDidUpdate = (e) => {
-  // console.log('Hello')
-  }
   // formSumbit = (event) => {
   //   event.preventDefault();
   // }
-
+  
   validateEmail(email) {
     var re = /\S+@\S+\.\S+/;
     if (re.test(email)) {
@@ -65,6 +64,7 @@ class Login extends Component {
   }
 
   render() {
+
     return (
       <div>
         {/* <TopNav /> */}
@@ -74,14 +74,14 @@ class Login extends Component {
                 <h1 className="login__title-top">Welcome,</h1>
                 <h2 className="login__title-sub">Sign in to continue!</h2>
               </div>
-              <div className="login__form">
+              <form className="login__form">
                 <label className="login__email-label" htmlFor="email">Email</label>
-                <input className="login__email" type="email" name="email" placeholder="Email" id="email"
+                <input className="login__email" type="email" name="email" placeholder="Email" id="email" autofocus="autofocus"
                   value={this.state.email}
                   onChange={this.handleInputChange} />
                   
                 <label className="login__password-label" htmlFor="password">Password</label>
-                <input className="login__password" type="password" name="password" placeholder="Password" id="password"
+                <input className="login__password" type="password" name="password" placeholder="Password" id="password" autofocus="autofocus"
                   value={this.state.password}
                   onChange={this.handleInputChange} />
                 <h3 className="forgot">Forgot Password?</h3>
@@ -97,7 +97,7 @@ class Login extends Component {
                   onClick={this.handleLoginGmail}>Connect with Gmail</button>
                 <button className="login__btn login__btn--login" type="submit" name="login" id="login"
                   onClick={this.handleLoginGitHub}>Connect with Github</button>
-              </div>
+              </form>
               <div className="new-user">
                 <h2 className="new-user__title">I'm a new user.</h2>
                 <Link className="link" to="/signup"><h2>Sign Up</h2></Link>
@@ -130,19 +130,3 @@ class Login extends Component {
 }
 
 export default Login;
-
-/* 
-// <form role="form">
-//   <div class="form-group">
-//     <label> Team/company name</label>
-//     <input type="text" name="team" autofocus="autofocus" class="form-control">
-//       <span class="invalid-feedback" style="display: none;">
-                
-//             </span></div> <div class="form-group"><label>Name</label> <input type="text" name="name" autofocus="autofocus" class="form-control"> <span class="invalid-feedback" style="display: none;">
-            
-//         </span></div> <div class="form-group"><label>Email Address</label> <input type="email" name="email" class="form-control"> <span class="invalid-feedback" style="display: none;">
-            
-//         </span></div> <div class="form-group"><label>Password</label> <input type="password" name="password" class="form-control"> <span class="invalid-feedback" style="display: none;">
-            
-//         </span></div> <div><button class="btn btn-primary mt-4 w-full"><span><i class="fa fa-btn fa-check-circle"></i> <span>Let's get started</span></span></button></div> <input type="hidden"> <input type="hidden"> <input type="hidden"> <input type="hidden"></form> 
-*/
