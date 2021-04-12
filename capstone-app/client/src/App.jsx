22cd35d780910626e54fc993b465233851872901
Cristian <cristian.niculescu@gmail.com>
2021-04-12 01:26:41 -0700

    passing back props via Route

--- capstone-app/client/src/App.jsx
+++ capstone-app/client/src/App.jsx
@@ -1,7 +1,11 @@
-// import React from 'react';
-// import './App.css';
 import { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import PropTypes from "prop-types";
+// import { withRouter } from 'react-router';
+import { API_URL } from './components/utils';
+import axios from 'axios';
+
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import MapArt from './pages/MapArt';
@@ -9,46 +13,50 @@ import Explore from './pages/Explore';
 // import Saved from './pages/Saved';
 import Details from './pages/Details';
 import Profile from './pages/Profile';
-import LayeredMap from './pages/LayeredMap/LayeredMap';
-import LayeredMapChloro from './pages/LayeredMapChloro/LayeredMapChloro';
-import PhotoGrid2x from './components/PhotoGrid2x/PhotoGrid2x'
-import DropDown from './components/DropDown/DropDown'
+import Gallery from './components/Gallery/Gallery'
 import PageNotFound from './components/PageNotFound/PageNotFound';
 
 
-// import axios from "axios"; <-- make call here and pass data down
+// Test pages: 
+// import LayeredMap from './pages/LayeredMap/LayeredMap';
+// import LayeredMapChloro from './pages/LayeredMapChloro/LayeredMapChloro';
 
-// Deploying with Git | Heroku Dev Center
-// const API_URL =
-//   process.env.NODE_ENV === "production"
-//     ? "https://git.heroku.com/name-of-project.git"
-//     : "http://localhost:8090";
-
-
-// import Test from './pages/Test';
-// import TestImg from './pages/TestImg';
-
-
-// <Route exact path='/login' component={Auth} />
-// <Route exact path='/browse' component={Browse} />  
-// Write art_works routes = use here less confusion, maybe same page can be loaded in two ways?
 class App extends Component {
+
   render() {
+
     return (
       <Router>
         <Switch>
           <Redirect exact from='/' to='/login' />
-          <Route exact path='/login' component={Login} />  
+          {/* <Route exact path='/login' component={Login} handleLogin={this.handleLogin()}/>   */}
+          {/* <Route exact path='/login' render={props => <Login {...props} />} /> */}
+          {/* <Route exact path='/login' render={props => <Login handleLogin = {this.handleLogin} />} /> */}
+          <Route exact path='/login'
+            render={props =>
+              <Login
+                handleLogin={this.handleLogin}
+                function={this.handleSaveUserOverHere}
+                // sendData={this.getData}
+              />
+            }
+          />
           <Route exact path='/signup' component={SignUp} />  
-          <Route exact path='/map' component={MapArt} />       
+          <Route path='/map' component={MapArt} />       
+          <Route path='/map'
+            render={props =>
+              <MapArt
+                user_id={this.state.user_id}
+              />
+            }
+          />
           <Route exact path='/art_works' component={Explore} />
           <Route exact path='/art_works/:id' component={Details} />
           {/* <Route exact path='/favourites' component={Saved} /> */}
           <Route exact path='/profile/:id' component={Profile} />
-          <Route exact path='/layered_map' component={LayeredMap} />
-          <Route exact path='/layered_map_chloro' component={LayeredMapChloro} />
-          <Route exact path='/photo-grid2x' component={PhotoGrid2x} />
-          <Route exact path='/dropdown' component={DropDown} />
+          {/* <Route exact path='/layered_map' component={LayeredMap} />
+          <Route exact path='/layered_map_chloro' component={LayeredMapChloro} /> */}
+          <Route exact path='/gallery' component={Gallery} />
           <Route exact path='/*' component={PageNotFound} />
         </Switch>
       </Router>
