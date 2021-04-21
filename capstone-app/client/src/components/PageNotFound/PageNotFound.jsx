7eed0bc1dcc8197711a612317a7b7326e0f96cc7
Cristian <cristian.niculescu@gmail.com>
2021-04-21 02:17:01 -0700

    finished matching login style

--- capstone-app/client/src/components/PageNotFound/PageNotFound.jsx
+++ capstone-app/client/src/components/PageNotFound/PageNotFound.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 function NotFound() {
   return (
-    <section className="not-found">
-      <div className="not-found__container">
-        <h1>404</h1>
-        <h1>This is not the page you are looking for,</h1>
-        <h1>go back or proceed to:</h1>
-        <h1><Link className="not-found__link" to="/login">Login</Link></h1>
+    <section className="nf">
+      <div className="nf__container">
+        <h1 className="nf__404"> -> 404 -> </h1>
+        <h1 className="nf__text">No art here,</h1>
+        <h1 className="nf__text">go back or proceed to</h1>
+        <h1><Link className="nf__link" to="/login">Login</Link></h1>
       </div>
     </section>
   )
